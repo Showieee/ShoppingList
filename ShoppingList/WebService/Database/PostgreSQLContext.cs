f9a597f51c13dfde8f8252d4ba88a38ac02b98a7
Antochi Silviu <justklayton@gmail.com>
2017-04-04 11:23:42 +0300

    Initial commit containing PostgreSQL EF Context

---
+++ ShoppingList/WebService/Database/PostgreSQLContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebService.Database
+{
+    public class PostgreSQLContext : DbContext
+    {
+        public PostgreSQLContext() : base(nameOrConnectionString: "PostgreSQLConnectionString")
+        {
+            System.Data.Entity.Database.SetInitializer<PostgreSQLContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ShoppingList/WebService/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebService.Models
+{
+    [Table("users", Schema = "public")]
+    public class User
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [Column("username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Column("password_hash")]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("telephone")]
+        public string Telephone { get; set; }
+
+        [Required]
+        [Column("is_driver")]
+        public bool IsDriver { get; set; }
+    }
+}
