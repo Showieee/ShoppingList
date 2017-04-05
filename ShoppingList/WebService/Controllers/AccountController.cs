4cc870a60da8c6a5119f83939a65ebf8a73df74c
Silviu Antochi <justklayton@gmail.com>
2017-04-05 08:43:33 +0300

    Added initial controller actions

---
+++ ShoppingList/WebService/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebService.Database;
+using WebService.Models;
+
+namespace WebService.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [HttpPost]
+        [Route("Login")]
+        [ResponseType(typeof(LoginInfo))]
+        public async Task<IHttpActionResult> Login(string username, string password)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            using (MainDbContext ctx = new MainDbContext())
+            {
+                try
+                {
+                    var loginInfo = await ctx.LoginInfo.FirstOrDefaultAsync(info => info.Username.Equals(username) && info.Password.Equals(password));
+                    if (loginInfo == null)
+                        return BadRequest("Invalid username or password");
+
+                    return Ok(loginInfo);
+                }
+                catch
+                {
+                    return BadRequest("Error while logging in");
+                }
+            }
+        }
+
+        [HttpPut]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(LoginInfo loginInfo)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            using (MainDbContext ctx = new MainDbContext())
+            {
+                try
+                {
+                    ctx.LoginInfo.Add(loginInfo);
+                    await ctx.SaveChangesAsync();
+
+                    return Ok();
+                }
+                catch
+                {
+                    return BadRequest("Error while registering");
+                }
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
