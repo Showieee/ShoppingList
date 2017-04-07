c23e54c718f80784dca094f37390d0840d0a27ee
Antochi Silviu <justklayton@gmail.com>
2017-04-07 11:36:30 +0300

    Test changed. Removed faulty project

--- ShoppingList/WebServiceHost/Config/WebApiConfig.cs
+++ ShoppingList/WebServiceHost/Config/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace WebServiceHost.Config
                 routeTemplate: "ShoppingList/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //TestChange
         }
     }
 }
